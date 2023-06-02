70064a892ba192d224c1645376835d6e4353447e
Daniel Carvalho <129884281+DanielPAdeCarvalho@users.noreply.github.com>
2023-06-02 16:49:46 -0300

    New Model

--- models/models.go
+++ models/models.go
@@ -1,6 +1,8 @@
 package models
 
 type PersyCoins struct {
-	Nome  string  `json:"Nome"`
-	Saldo float64 `json:"saldo"`
+	Nome      string  `json:"Nome"`
+	Sobrenome string  `json:"sobrenome"`
+	Email     string  `json:"email"`
+	Saldo     float64 `json:"saldo"`
 }
